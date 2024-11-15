2bd7cb95c5849651aac91177ca3dd2357cfd4f34
QuangNV <nguyenvquang1611@gmail.com>
2024-11-15 22:40:23 +0700

    add google service deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
 
     kotlin("plugin.serialization") version "2.0.20"
+//    id("com.google.gms.google-services")
 }
 
 android {
--- app/src/main/java/dev/proptit/kotlinflow/service/ChatService.kt
+++ app/src/main/java/dev/proptit/kotlinflow/service/ChatService.kt
@@ -3,6 +3,7 @@ package dev.proptit.kotlinflow.service
 import dev.proptit.kotlinflow.domain.Chat
 
 class ChatService() {
+//    private val firebase = Firebase.firestore
     suspend fun getUserChats(uid: String): List<Chat> {
         return listOf(
             Chat(
